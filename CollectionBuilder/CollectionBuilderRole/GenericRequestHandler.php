16cad25caa905630a9821d39c1a1c1e902f9561d
Ravindra <ravindra_chandrashekar@mindtree.com>
2011-02-16 01:03:25 +0530

    Minor changes

--- CollectionBuilder/CollectionBuilderRole/GenericRequestHandler.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-header( "HTTP/1.1 301 Moved Permanently" );
-header( "Status: 301 Moved Permanently" );
-$qs = $_SERVER['QUERY_STRING'];
-header( "Location: GenericRequestHandler.aspx?$qs" );
-exit(0);
-?>
